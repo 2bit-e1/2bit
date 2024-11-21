fcae0af7a8a6d1079caa364081a2b288f3435d99
Sega-hub <serdjefre@gmail.com>
2024-11-21 17:10:33 +0300

    fix rollup bag

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ const root = resolve(__dirname, "./src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@dotlottie/player-component'],
+    },
+  },
   plugins: [
     vue({
       template: {
