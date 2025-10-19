a28e57388f11319345d0565e3cc441f8db230058
2bit-e1 <kakpechorin666@gmail.com>
2025-10-19 22:49:40 +0300

    Update 7.js

--- src/data/projects/7.js
+++ src/data/projects/7.js
@@ -3,8 +3,8 @@ import { CONTENT_TYPES } from "@/utils/constants";
 export default {
   name: "Gitlife",
   slug: "gitlife",
-  roles: ["Дизайн", "Визуальная идентификация", "Гайдлайн"],
-  year: 29,
+  roles: ["3D", "Визуальная идентификация", "Веб" "Анимация"],
+  year: 24,
   number: 7,
   content: [
     {
