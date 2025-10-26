1bc3aa18f919722e78acb28f4b63b972d5e52ff1
2bit-e1 <kakpechorin666@gmail.com>
2025-10-26 19:11:37 +0300

    Update me.js

--- src/data/me.js
+++ src/data/me.js
@@ -38,7 +38,7 @@ export default {
   content: [
     {
       type: CONTENT_TYPES.iframe,
-      link: "bio.png", //3.1.webp / 3.2.mp4
+      link: "bio.png"
     },
     {
       type: CONTENT_TYPES.paragraph,
