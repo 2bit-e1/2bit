aaba9814d5c69723cdb4c19d7e2eb7a06bab5aec
2bit-e1 <kakpechorin666@gmail.com>
2026-06-27 22:24:52 +0300

    Update 8.js

--- src/data/projects/8.js
+++ src/data/projects/8.js
@@ -10,7 +10,7 @@ export default {
     {
       type: CONTENT_TYPES.paragraph,
       text: `
-        In 2020 we were approached by a founder of uprising hedge fund to help him develop branding. The client's main request was to create a trusted image for the fund with deep expertise.
+        -
       `,
     },
     {
